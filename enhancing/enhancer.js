eae675e64e54d75678247ef8b9043bc9ea96bb62
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-24 17:33:20 -0600

    mvp met

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -12,27 +12,38 @@ module.exports = {
 // }
 
 function success(item) {
-  if(item.enhancement<20){
-    let enhancedItem= { ...item,enhancement:item.enhancement+=1 };
+  if (item.enhancement < 20) {
+    let enhancedItem = { ...item, enhancement: (item.enhancement += 1) };
     return enhancedItem;
-  }else{
-    let enhancedItem= { ...item};
+  } else {
+    let enhancedItem = { ...item };
     return enhancedItem;
   }
 }
-  
-  
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement<15){
+    let failItem = {...item,durability:(item.durability-=5)};
+    return failItem;
+  }else if(item.enhancement<15){
+    let failItem = { ...item, durability:(item.durability-=10)}
+    return failItem;
+  }else{
+    let failItem= {
+      ...item, 
+      durability:(item.durability-=10),
+      enhancement:(item.enhancement-=1)
+    }
+    return failItem;
+  }
+  
 }
 
 function repair(item) {
-  let repairedItem= { ...item, durability:item.durability=100 };
+  let repairedItem = { ...item, durability: (item.durability = 100) };
   return repairedItem;
 }
 
 function get(item) {
   return { ...item };
 }
-
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -18,19 +18,43 @@ describe("enhancer item methods", () => {
     expect(enhancer).toBeDefined();
   });
 
-  it("enhancer repairs an items durability to 100", () => {
+  it("Repair method repairs an items durability to 100", () => {
     enhancer.repair(item);
     expect(item.durability).not.toBe(10);
     expect(item.durability).toBe(100);
   });
-  it("success method increases enchantment by 1",()=>{
+  it("On success, the item's enhancement increases by 1.", () => {
+    enhancer.success(item);
+    expect(item.enhancement).toEqual(4);
+  });
+  it("On success,if the item enhancement level is 20, the enhancement level is not changed.", () => {
+    if ((item.enhancement = 20)) {
       enhancer.success(item);
-      expect(item.enhancement).toEqual(4)
-  })
-  it("if enhancement level is max, does not increase",()=>{
-    if(item.enhancement=20){
-        enhancer.success(item)
-        expect(item.enhancement).not.toBeGreaterThan(20);
-    }  
+      expect(item.enhancement).not.toBeGreaterThan(20);
+    }
+  });
+  it("On success,the durability of the item is not changed.", () => {
+    enhancer.success(item);
+    expect(item.durability).toEqual(item.durability);
+  });
+  it("On fail,if the item's enhancement is less than 15, the durability of the item is decreased by 5.", () => {
+    enhancer.fail(item);
+    if (item.enhancement < 15) {
+      expect(item.durability).toBe(5);
+    }
+  });
+  it("On fail,if the item's enhancement is 15 or more, the durability of the item is decreased by 10.", () => {
+    item = { ...item, enhancement: 15 };
+    enhancer.fail(item);
+    if (item.enhancement >= 15) {
+      expect(item.durability).toBe(0);
+    }
+  });
+  it("On fail,if the item's enhancement level is greater than 16, the enhancement level decreases by 1",()=>{
+      item={...item,enhancement:18};
+      enhancer.fail(item);
+      if(item.enhancement>15){
+          expect(item.enhancement).toBe(17)
+      }
   })
 });
